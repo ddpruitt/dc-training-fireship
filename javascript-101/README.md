c5363c1fb7e61d88b85432e754153a8705187aa0
darren <Darren.Pruitt@energytransfer.com>
2024-12-27 14:52:53 -0600

    changes

---
+++ javascript-101/README.md
@@ -0,0 +1,3 @@
+# Fireship JS
+
+[Resources](https://fireship.io/courses/js/101-resources/)
---
+++ javascript-101/app.js
@@ -0,0 +1,7 @@
+console.log('Hello, World! 🌐');
+
+const range = Array(100).fill(0).map((_, i) => i + 1);
+console.log(range);
+
+const range2 = [...Array(100).keys()];
+console.log(range2);
